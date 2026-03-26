05862b8af56c1305ca9f829a1ff7d28bc3648c90
nimosana <126646493+nimosana@users.noreply.github.com>
2026-03-25 21:51:03 -0400

    portrait 4:3, paragraph font size and prep bio page

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
     const navToggle = document.querySelector(".nav-toggle");
     const navBar = document.querySelector(".navbar");
-    
+
     if (navToggle && navBar) {
         navToggle.addEventListener("click", () => {
             const isExpanded = navToggle.getAttribute("aria-expanded") === "true";
