44a630122ca4b70456792ec485b52ccf122c53e1
nimosana <126646493+nimosana@users.noreply.github.com>
2026-04-13 21:13:00 -0400

    bouta reorganize

--- script.js
+++ script.js
@@ -59,4 +59,19 @@ document.addEventListener("DOMContentLoaded", () => {
             });
         });
     });
+
+    // Modal Backdrop Clicking Handler
+    document.querySelectorAll('dialog.project-modal').forEach(dialog => {
+        dialog.addEventListener('click', (e) => {
+            const dialogDimensions = dialog.getBoundingClientRect();
+            if (
+                e.clientX < dialogDimensions.left ||
+                e.clientX > dialogDimensions.right ||
+                e.clientY < dialogDimensions.top ||
+                e.clientY > dialogDimensions.bottom
+            ) {
+                dialog.close();
+            }
+        });
+    });
 });
